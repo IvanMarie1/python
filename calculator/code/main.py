251f8e94364d3c74ad88954bdc5f4678bb220dd1
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2024-01-27 23:58:50 +0100

    Update main.py

--- calculator/code/main.py
+++ calculator/code/main.py
@@ -1,1 +1,13 @@
-# TODO merge calc and graphic
+from graphic import create_calc
+from tkinter import *
+
+
+def main():
+    my_calc = Tk()
+    create_calc(my_calc)
+
+    my_calc.mainloop()
+
+
+if __name__ == "__main__":
+    main()
