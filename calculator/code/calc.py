80dcf6d1d15179105f566716afc463a249473ad2
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2024-01-29 20:16:25 +0100

    Update calc.py
    Support parenthesis
    Reorganization of the calculation function

--- calculator/code/calc.py
+++ calculator/code/calc.py
@@ -1,78 +1,134 @@
 import re
 
 
+def operation(a: float, b: float, operator: str) -> float:
+    """Return the result of a basic operation
+    
+    Only [ + - * / ] supported"""
+    result = 0
+    if operator == "+":
+        result = a + b
+    elif operator == "-":
+        result = a - b
+    elif operator == "*":
+        result = a * b
+    elif operator == "/":
+        try:
+            result = a / b
+        except ZeroDivisionError:
+            print("You can't divide by zero")
+    return result
+
+def closed_parenthesis(text, i_start) -> int:
+    """Return the index at which the parenthesis is closed, if not raise an error"""
+    i_end = i_start + 1
+
+    while i_end < len(text):
+        if text[i_end] == "(":
+            i_end = closed_parenthesis(text, i_end)
+        elif text[i_end] == ")":
+            return i_end
+        i_end += 1
+    
+    raise SyntaxError('Parenthesis not closed')
+
+def simplify(number: str) -> int | float:
+    """Convert a string number into int or float according to its value
+    
+    If it is not a number, return 0"""
+    try:
+        result = float(number)
+        if result.is_integer():
+            return int(result)
+        return result
+    except ValueError:
+        print("Invalid Syntax")
+        return 0
+
+def split_calculation(calc: str) -> list[str]:
+    """Split a calculation in different element : parenthesis until it's closed, numbers, operators
+    
+    Return a list of the calcultion's terms"""
+    terms = []
+    i_start = 0
+    i_end = 0
+
+    while i_end < len(calc):
+        if calc[i_end] == '(':
+            # append what is before the parenthesis
+            terms += my_split(calc, i_start, i_end)
+            # append the content of the parenthesis
+            i_start = i_end
+            i_end = closed_parenthesis(calc, i_end)
+            terms.append(calc[i_start:i_end+1])
+            i_start = i_end+1
+        i_end += 1
+    # append the last term
+    terms += my_split(calc, i_start, i_end)
+    return terms
+
+
+def my_split(text, i_start, i_end):
+    """Split a calculation with re module
+
+    Return a list of the splited numbers and operators"""
+    my_pattern = r'(\+|(?<![/*+])-|\/|\*)'
+    my_first_pattern = r'(\+|(?<![/*+])(?!^-)-|\/|\*)'
+
+    if i_start == 0:
+        return ' '.join(re.split(my_first_pattern, text[:i_end])).split() # handle first neg number
+    return ' '.join(re.split(my_pattern, text[i_start:i_end])).split()
+
+
+
+
 def calculate(calc: str) -> float:
     """Return the result of a calculation string
 
-    Only support + - * /
+    Only support [ + - * / ]
 
     ---
     TODO 
-    - add parenthesis
     - add other operators like exponent, sqrt, etc.
+    - "intelligent" caluculations like (2+3)(2-3) without operator
     """
-    supported_char = list('0123456789+-*/.')
 
+    # only supported characters
+    supported_char = list('0123456789+-*/.()')
     for char in calc:
         if char not in supported_char:
             print("Unknown character")
             return 0
-
-    priority = {"*": 1, "/": 1, "+": 2, "-": 2}
-
-    my_pattern = r'(\+|(?!^-)(?<![/*+-])-|\/|\*)'
-    terms  = re.split(my_pattern, calc) # split the operators but not a - at the start
     
+    # if it is englobed by parentheses, remove them
+    if calc.startswith('('):
+        if closed_parenthesis(calc, 0) == len(calc) - 1:
+            calc = calc[1:-1]
 
-    for term in terms:
-        if term == "":
-            print("Invalid Syntax")
 
+    # splitting the calculation
+    terms = split_calculation(calc)
     if len(terms) <= 1:
-        try:
-            result = float(calc)
-            if result.is_integer():
-                return int(result)
-            return result
-        except ValueError:
-            print("Invalid Syntax")
-            return 0
+        return simplify(calc)
 
     # finding the last operation
+    priority = {"*": 1, "/": 1, "+": 2, "-": 2}
     max_priority = 0
-    last_operator = 0
+    i_operator = 0
     for i in range(len(terms)):
         if terms[i] in '+-*/':
             if priority[terms[i]] >= max_priority:
                 max_priority = priority[terms[i]]
-                last_operator = i
+                i_operator = i
     
-    operator = terms[last_operator]
-    left_term = calculate(''.join(terms[:last_operator]))
-    right_term = calculate(''.join(terms[last_operator+1:]))
+
+    operator = terms[i_operator]
+    left_term = calculate(''.join(terms[:i_operator]))
+    right_term = calculate(''.join(terms[i_operator+1:]))
     return operation(left_term, right_term, operator)
         
 
 
-def operation(a: float, b: float, operator: str) -> float:
-    result = 0
-    match operator:
-        case "+":
-            result = a + b
-        case "-":
-            result = a - b
-        case "*":
-            result = a * b
-        case "/":
-            try:
-                result = a / b
-            except ZeroDivisionError:
-                print("You can't divide by 0")
-    return result
-
-
-def main():
-    print(calculate('1.3*-5-48'))
 
 if __name__ == "__main__":
-    main()
+    print(calculate('-5+(5+3)-5'))
