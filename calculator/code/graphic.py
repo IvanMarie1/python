fa2df1cc877cadd7374ceb2de3a68ffae3b3ff9a
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2024-01-29 20:18:48 +0100

    Update graphic.py
    move config_button code to create_calc to make it easier to understand
    
    changed a bit the calculator interface

--- calculator/code/graphic.py
+++ calculator/code/graphic.py
@@ -3,6 +3,8 @@ from functools import partial
 from calc import calculate
 
 
+
+# functions for buttons
 def change_screen(screen_text: StringVar, char: str):
     screen_text.set(screen_text.get() + char)
 
@@ -12,98 +14,70 @@ def clear_screen(screen_text: StringVar):
 def del_screen(screen_text: StringVar):
     screen_text.set(screen_text.get()[:-1])
 
-def keep_flat(event): 
-    event.widget.config(relief=FLAT) 
-
 def output_result(text: StringVar):
     result = calculate(text.get())
     text.set(str(result))
 
 
-def create_calc() -> tuple[Tk, StringVar]:
-    UNIT = 30
-    my_font = 'Courier 18 bold'
 
+def create_calc() -> tuple[Tk, StringVar]:
+    """Create a calculator with Tkinter
+        
+    Returns the top-window widget and the text-variable displayed in the calculator screen"""
+
+    # setting up the top-window
     root = Tk()
     root.title("Calculator")
     root.resizable(False, False)
 
 
-    frame = Canvas(root, bg="#2b2d42", width=15*UNIT, height=22*UNIT)
-    frame.grid(sticky='nsew', columnspan=15, rowspan=22)
+    # frame with the size
+    UNIT = 35
+    frame = Canvas(root, bg="#2b2d42", width=10*UNIT, height=16*UNIT)
+    frame.grid(sticky='nsew', columnspan=10, rowspan=16)
 
+
+    # calculator screen
     text = StringVar()
+    my_font = 'Courier 16 bold'
     screen = Label(root, textvariable=text, bg="#edf2f4", relief=FLAT, padx=UNIT, anchor=W, font=my_font, width=1, name="screen")
-    screen.grid(column=1, row=1, columnspan=13, rowspan=3, sticky="nsew")
+    screen.grid(column=1, row=1, columnspan=8, rowspan=3, sticky="nsew")
 
 
-    # adding number keys
-    nums = [Button(
-        root, text=str(9-i), bg="#8d99ae", activebackground='#79869c', relief=FLAT, name=f"btn-{9-i}", font=my_font
-        ) for i in range(10)]
-    for i in range(9):
-        row, col, = 9+(i//3)*3, 8-(i%3)*3
-        nums[i].grid(row=row, column=col, columnspan=2, rowspan=2, sticky="nsew")
-    nums[-1].grid(row=18, column=2,columnspan=5, rowspan=2, sticky="nsew")
+    # all the buttons
+    my_buttons = [
+        {"text": "C", "bg": "#ef233c", "act-bg": "#d90429", "cmd":partial(clear_screen, text)},
+        {"text": "del", "bg": "#ef233c", "act-bg": "#d90429", "cmd":partial(del_screen, text)},
+        {"text": "(", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '(')},
+        {"text": ")", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, ')')},
+        {"text": "7", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '7')},
+        {"text": "8", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '8')},
+        {"text": "9", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '9')},
+        {"text": "/", "bg": "#edf2f4", "act-bg": "#ced4da", "cmd": partial(change_screen, text, '/')},
+        {"text": "4", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '4')},
+        {"text": "5", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '5')},
+        {"text": "6", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '6')},
+        {"text": "*", "bg": "#edf2f4", "act-bg": "#ced4da", "cmd": partial(change_screen, text, '*')},
+        {"text": "1", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '1')},
+        {"text": "2", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '2')},
+        {"text": "3", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '3')},
+        {"text": "-", "bg": "#edf2f4", "act-bg": "#ced4da", "cmd": partial(change_screen, text, '-')},
+        {"text": "0", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '0')},
+        {"text": ".", "bg": "#8d99ae", "act-bg": "#79869c", "cmd": partial(change_screen, text, '.')},
+        {"text": "=", "bg": "#ef233c", "act-bg": "#d90429", "cmd":partial(output_result, text)},
+        {"text": "+", "bg": "#edf2f4", "act-bg": "#ced4da", "cmd": partial(change_screen, text, '+')}
+    ]
 
-    # operator keys
-    operators = [Button(
-        root, text=op, bg="#edf2f4", activebackground='#ced4da', relief=FLAT, name=f"btn-{name}", font=my_font
-        ) for op, name in zip('/*-+', ['div', 'mult', 'sub', 'add'])]
-    positions = [(6, 8), (6, 11), (9, 11), (12, 11)]
-    for i in range(4):
-        row, col = positions[i]
-        operators[i].grid(row=row, column=col,columnspan=2, rowspan=2, sticky="nsew")
+    for i in range(len(my_buttons)):
+        btn = my_buttons[i]
+        temp_btn = Button(root, text=btn['text'], bg=btn['bg'], activebackground=btn['act-bg'], font=my_font, command=btn['cmd'])
+        temp_btn.grid(row=5+(i//4)*2, column=1+(i%4)*2, columnspan=2, rowspan=2, sticky="nsew")
 
-    # point, equal, del, clear keys
-    point = Button(root, text=".", bg="#8d99ae", activebackground='#79869c', relief=FLAT, name=f"btn-point", font=my_font)
-    point.grid(row=18, column=8,columnspan=2, rowspan=2, sticky="nsew")
-
-    equal = Button(root, text="=", bg="#ef233c", activebackground='#d90429', relief=FLAT, name=f"btn-equal", font=my_font)
-    equal.grid(row=15, column=11,columnspan=2, rowspan=5, sticky="nsew")
-
-    del_btn = Button(root, text="del", bg="#ef233c", activebackground='#d90429', relief=FLAT, name=f"btn-del", font=my_font)
-    del_btn.grid(row=6, column=5,columnspan=2, rowspan=2, sticky="nsew")
-
-    clear = Button(root, text="C", bg="#ef233c", activebackground='#d90429', relief=FLAT, name=f"btn-clear", font=my_font)
-    clear.grid(row=6, column=2,columnspan=2, rowspan=2, sticky="nsew")
-
-    root.bind('<Button-1>', keep_flat)
 
     return (root, text)
 
 
-def config_button(root: Tk, text: StringVar):
-    config = {
-        'btn-1': partial(change_screen, text, '1'),
-        'btn-2': partial(change_screen, text, '2'),
-        'btn-3': partial(change_screen, text, '3'),
-        'btn-4': partial(change_screen, text, '4'),
-        'btn-5': partial(change_screen, text, '5'),
-        'btn-6': partial(change_screen, text, '6'),
-        'btn-7': partial(change_screen, text, '7'),
-        'btn-8': partial(change_screen, text, '8'),
-        'btn-9': partial(change_screen, text, '9'),
-        'btn-0': partial(change_screen, text, '0'),
-        'btn-point': partial(change_screen, text, '.'),
-        'btn-div': partial(change_screen, text, '/'),
-        'btn-mult': partial(change_screen, text, '*'),
-        'btn-sub': partial(change_screen, text, '-'),
-        'btn-add': partial(change_screen, text, '+'),
-        'btn-equal': partial(output_result, text),
-        'btn-del': partial(del_screen, text),
-        'btn-clear': partial(clear_screen, text)
-    }
-    for child in root.children:
-        if child.startswith('btn'):
-            root.nametowidget(child).config(command=config[child])
-
-
-def main():
-    calc, text = create_calc()
-    config_button(calc, text)
-    
-    calc.mainloop()
 
 if __name__ == "__main__":
-    main()
+    calc, text = create_calc()
+    calc.mainloop()
